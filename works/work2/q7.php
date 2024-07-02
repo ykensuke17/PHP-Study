50c834c44b15de909f1fda7792833d4e96529db2
302A-18\cre <a>
2024-07-02 15:05:19 +0900

    q7クリア

--- works/work2/q7.php
+++ works/work2/q7.php
@@ -9,3 +9,14 @@
 // 出力例 「johnの評価は良です」
 
 $scores = array("John" => 85, "Jane" => 92, "Jim" => 78);
+foreach ($scores as $name => $score) {
+    if ($score >= 90) {
+        echo "$name の評価は優です\n";
+    } elseif ($score >= 80 && $score < 90) {
+        echo "$name の評価は良です\n";
+    } elseif ($score >= 70 && $score < 80) {
+        echo "$name の評価は可です\n";
+    } else {
+        echo "$name の評価は不可です\n";
+    }
+}
