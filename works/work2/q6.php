9ec582d73ca3b255ffb24f495d7f70196c3dd361
302A-18\cre <a>
2024-07-02 14:37:33 +0900

    q6クリア

--- works/work2/q6.php
+++ works/work2/q6.php
@@ -2,3 +2,9 @@
 
 // 以下の配列をの要素のうち文字数が偶数のもののみ表示するループを書いてください
 $fruits = array("Apple", "Banana", "Cherry");
+for ($i = 0; $i < count($fruits); $i++) {
+    $length = mb_strlen($fruits[$i]);
+    if ($length % 2 == 0) {
+        echo "$fruits[$i]\n";
+    }
+}
