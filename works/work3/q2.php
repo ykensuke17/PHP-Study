efc00ac2e27d76ec031b1a6a49e3b4e81b37724f
302A-18\cre <a>
2024-07-02 16:09:53 +0900

    q2クリア

--- works/work3/q2.php
+++ works/work3/q2.php
@@ -2,10 +2,11 @@
 
 // 引数として受け取った2つの数値を掛け算して返す関数を完成させなさい
 
-$result = multiply();
+$result = multiply(3, 3);
 echo "結果: " . $result . "\n";
 
 
-function multiply()
+function multiply($x, $y)
 {
+    return $x * $y;
 }
